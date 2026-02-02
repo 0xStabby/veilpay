0b5e9be4f9a3d3361adcc2fb6dfaa93e231857d2
0xstabby <0xstabby@protonmail.com>
2026-02-02 09:02:58 -0800

    re-add burner wallet option in wallet connect

--- app/src/components/WalletProviders/WalletProviders.tsx
+++ app/src/components/WalletProviders/WalletProviders.tsx
@@ -2,6 +2,7 @@ import { useMemo } from 'react';
 import type { FC, ReactNode } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-unsafe-burner';
 import { LOCALNET_RPC } from '../../lib/config';
 
 import '@solana/wallet-adapter-react-ui/styles.css';
@@ -12,7 +13,7 @@ type WalletProvidersProps = {
 
 export const WalletProviders: FC<WalletProvidersProps> = ({ children }) => {
     const endpoint = useMemo(() => LOCALNET_RPC, []);
-    const wallets = useMemo(() => [], []);
+    const wallets = useMemo(() => [new UnsafeBurnerWalletAdapter()], []);
 
     return (
         <ConnectionProvider endpoint={endpoint}>
